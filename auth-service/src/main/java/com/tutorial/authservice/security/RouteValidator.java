c5cba1411d3ca58278c3ec42c1b7519858351f05
cjesus123 <huancocristian@gmail.com>
2024-07-10 01:14:16 -0300

    correccion en el path v4

--- auth-service/src/main/java/com/tutorial/authservice/security/RouteValidator.java
+++ auth-service/src/main/java/com/tutorial/authservice/security/RouteValidator.java
@@ -6,7 +6,6 @@ import lombok.Setter;
 import org.apache.logging.log4j.message.ReusableMessage;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
-import org.springframework.util.AntPathMatcher;
 
 import java.util.List;
 import java.util.regex.Pattern;
@@ -17,37 +16,16 @@ import java.util.regex.Pattern;
 @ConfigurationProperties(prefix = "paths")
 public class RouteValidator {
 
-    private List<RequestDto> admin;
     private List<RequestDto> directorCarrera;
     private List<RequestDto> bedelia;
 
-    public boolean isAdminPath(RequestDto dto) {
-        System.out.println("ENTRO A TESTEAR RUTAS DE ADMIN ");
-        for (RequestDto d : admin) {
-            System.out.println(d.getUri());
-        }
-        return admin.stream().anyMatch(p ->
-                Pattern.compile(p.getUri()).matcher(dto.getUri()).matches() &&
-                        Pattern.compile(p.getMethod()).matcher(dto.getMethod()).matches());
-    }
-
     public boolean isDirectorCareer(RequestDto dto) {
-        System.out.println("ENTRO A TESTEAR RUTAS DE DIRECTOR");
-        for (RequestDto d : directorCarrera) {
-            System.out.println(d.getUri());
-        }
         return directorCarrera.stream().anyMatch(p ->
-                Pattern.compile(p.getUri()).matcher(dto.getUri()).matches() &&
-                        Pattern.compile(p.getMethod()).matcher(dto.getMethod()).matches());
+                Pattern.matches(p.getUri(), dto.getUri()) && p.getMethod().equals(dto.getMethod()));
     }
 
     public boolean isBedelia(RequestDto dto) {
-        System.out.println("ENTRO A TESTEAR RUTAS DE BEDELIA");
-        for (RequestDto d : bedelia) {
-            System.out.println(d.getUri());
-        }
-        AntPathMatcher matcher = new AntPathMatcher();
-        return bedelia.stream().anyMatch(p ->
-                matcher.match(p.getUri(), dto.getUri()) && p.getMethod().equals(dto.getMethod()));
+        return directorCarrera.stream().anyMatch(p ->
+                Pattern.matches(p.getUri(), dto.getUri()) && p.getMethod().equals(dto.getMethod()));
     }
 }
